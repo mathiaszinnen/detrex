997b00a9feba6dec8462665eebe82f2670800dcb
Mathias <mathias.zinnen@gmail.com>
2023-05-29 11:38:37 +0200

    resolve name confusion

--- projects/dino/configs/rn50_in1k_timm.py
+++ projects/dino/configs/rn50_in1k_timm.py
@@ -1,11 +1,10 @@
+from detrex.config import get_config
 from detectron2.config import LazyCall as L
 from detectron2.modeling import ShapeSpec
 from detectron2.layers import FrozenBatchNorm2d
+
 from .dino_r50_4scale_12ep import (
     train,
-    dataloader,
-    optimizer,
-    lr_multiplier,
 )
 from .models.dino_r50 import model
 
@@ -13,7 +12,7 @@ from detrex.modeling.backbone import TimmBackbone
 
 # modify backbone configs
 model.backbone = L(TimmBackbone)(
-    model_name="resnet152d",  # name in timm
+    model_name="resnet50",  # name in timm
     features_only=True,
     pretrained=True,
     in_channels=3,
@@ -28,6 +27,21 @@ model.neck.input_shapes = {
     "p3": ShapeSpec(channels=1024),
 }
 model.neck.in_features = ["p1", "p2", "p3"]
+model.num_classes = 139
+
+lr_multiplier = get_config("common/coco_schedule.py").lr_multiplier_36ep
+
+EPOCH_ITERS = 4264
+
+train.max_iter = 50 * EPOCH_ITERS
+train.output_dir = "/home/woody/iwi5/iwi5064h/backbone_ablation/rn50_in1k_timm"
+
+train.eval_period = EPOCH_ITERS
+train.checkpointer.period = 10 * EPOCH_ITERS
+
+train.final_checkpoint = "/home/woody/iwi5/iwi5064h/backbone_ablation/rn50_in1k_timm/final"
+
+model.backbone.drop_path_rate = 0.4
 
 # modify training configs
 train.init_checkpoint = ""
